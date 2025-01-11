cd016dedae577de9475663eaab1e1a8296094f0f
JavGarin <hlm.javiergarin@gmail.com>
2025-01-11 16:12:40 -0300

    add src/functions

---
+++ src/functions.js
@@ -0,0 +1,8 @@
+export const genTitle = () => 'Curso de Node JS';
+
+export const getAuthor = (author) => `El autor del curso es ${author}`
+
+//module.exports = {
+    //genTitle,
+    //getAuthor
+//}
