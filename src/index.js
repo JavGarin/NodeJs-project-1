6c0f55d673e01a819a8ac8c5254fd663d18215cd
JavGarin <hlm.javiergarin@gmail.com>
2025-01-11 16:11:06 -0300

    add src/index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log("Hola Mundo desde Index.js");
+//require('./functions')
+//require('./objects')
+
+//console.log(functions.gentitle())
+//console.log(functions.getAuthor('Javier Garin'))
+
+import {genTitle, getAuthor} from './functions.js'
+import {curso} from './objects.js'
+
+//const { genTitle, getAuthor } = require('./functions')
+//const { curso } = require('./objects')
+
+const {nombre, autor} = curso
+
+console.log(genTitle())
+console.log(getAuthor('Javier Garin'))
+console.log(nombre)
+console.log(autor)
