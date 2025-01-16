c3c2134210c9b87053c891a21a67061a1ddba1a6
JavGarin <hlm.javiergarin@gmail.com>
2025-01-16 10:40:23 -0300

    src/index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,27 @@
+// forma antigua de importar
 //require('./functions')
 //require('./objects')
 
 //console.log(functions.gentitle())
 //console.log(functions.getAuthor('Javier Garin'))
 
-import {genTitle, getAuthor} from './functions.js'
-import {curso} from './objects.js'
-
+// forma legacy de importar
 //const { genTitle, getAuthor } = require('./functions')
 //const { curso } = require('./objects')
 
-const {nombre, autor} = curso
+//-------------------------------------------------------------
 
-console.log(genTitle())
-console.log(getAuthor('Javier Garin'))
-console.log(nombre)
-console.log(autor)
+// forma nueva de importar
+//import {genTitle, getAuthor} from './functions.js'
+//import {curso} from './objects.js'
+
+
+// destructuring
+//const {nombre, autor} = curso
+
+//console.log(genTitle())
+//console.log(getAuthor('Javier Garin'))
+//console.log(nombre)
+//console.log(autor)
+
+console.log (process)
