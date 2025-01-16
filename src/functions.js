9f043c3fcd62d5ddbe28fa4827ab482faa044bab
JavGarin <hlm.javiergarin@gmail.com>
2025-01-16 10:39:43 -0300

    src/functions.js

--- src/functions.js
+++ src/functions.js
@@ -1,7 +1,12 @@
+// forma nueva de exportar ES6
 export const genTitle = () => 'Curso de Node JS';
 
 export const getAuthor = (author) => `El autor del curso es ${author}`
 
+
+//---------------------------------------------
+
+// forma antigua de exportar
 //module.exports = {
     //genTitle,
     //getAuthor
