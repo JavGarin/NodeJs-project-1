15cfa42f036c8f33ae2e908e9623395f93412912
JavGarin <hlm.javiergarin@gmail.com>
2025-01-16 10:41:33 -0300

    src/objects.js

--- src/objects.js
+++ src/objects.js
@@ -1,8 +1,10 @@
+// Exportar un objeto, forma nueva
 export const curso = {
     nombre: 'Curso de Node JS',
     autor: 'Javier Garin',
 }
 
+// forma antigua de exportar
 //module.exports = {
 //    curso 
 //}
